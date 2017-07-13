26f3df2f82c7b3de5ae9ae86157d7370a787ccdf
tapi-y <tapi09ucat@gmail.com>
2017-07-13 21:59:05 +0900

    sample src remake

--- script/sample.py
+++ script/sample.py
@@ -9,9 +9,8 @@ from abstractBot import *
 from geometry_msgs.msg import Twist
 
 class SampleBot(AbstractBot):
-    
     def strategy(self):
-        r = rospy.Rate(100) #100Hz
+        r = rospy.Rate(100)
 
         control_speed = 0
         control_turn = 0
@@ -22,25 +21,23 @@ class SampleBot(AbstractBot):
         while not rospy.is_shutdown():
             if self.center_bumper or self.left_bumper or self.right_bumper:
                 update_time = time.time()
-                rospy.loginfo('bumper hit!!')
-                control_speed = 0
-                control_turn = 1.5
+                control_speed = -0.5
+                control_turn = 0
             
             elif time.time() - update_time > UPDATE_FREQUENCY:
                 update_time = time.time()
                 
                 value = random.randint(1,1000)
                 if value < 500:
-                	control_speed = 0.5
+                	control_speed = 0.3
                 	control_turn = 1
                 else:
-                	control_speed = 0.5
+                	control_speed = 0.3
                 	control_turn = -1
 
             twist = Twist()
             twist.linear.x = control_speed; twist.linear.y = 0; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = control_turn
- 
             self.vel_pub.publish(twist)
 
             r.sleep()
